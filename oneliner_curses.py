423815334223a3e02f539565ee3837eb6359a56e
m!nus <minus@mnus.de>
2012-07-03 20:26:45 +0200

    OnelinerCurses fixed a bit

--- oneliner_curses.py
+++ oneliner_curses.py
@@ -1,6 +1,6 @@
 #import Oneliner from oneliner
 import logging
-import curses
+import curses, curses.wrapper
 
 class OnelinerCurses(object):
 	def __init__(self, baseUrl, historyLength=500):
@@ -8,6 +8,7 @@ class OnelinerCurses(object):
 		
 		self.commandHistory = []
 		self.commandIndex = 0
+		self.inputBuffer = ""
 		
 		self.log = logging.getLogger(self.__class__.__name__)
 		
@@ -16,28 +17,38 @@ class OnelinerCurses(object):
 		self.log.info("Oneliner curses frontend initialized")
 	
 	def InitCurses(self):
-		# initialize standard screen
-		self.screen = curses.initscr()
-		# don't echo key input
-		curses.noecho()
-		# turn off buffered mode (so the program
-		# receives key presses without waiting for enter)
-		curses.cbreak()
-		# enable handling of special keys (like arrow keys)
-		# in curses
-		self.screen.keypad(1)
-		# enable colors
-		curses.start_color()
+		try:
+			# initialize standard screen
+			self.screen = curses.initscr()
+			# don't echo key input
+			curses.noecho()
+			# turn off buffered mode (so the program
+			# receives key presses without waiting for enter)
+			curses.cbreak()
+			# enable handling of special keys (like arrow keys)
+			# in curses
+			#self.screen.keypad(1)
+			# enable colors
+			curses.start_color()
+			
+			curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+			self.inputColor = curses.color_pair(1)
 		
-		# create the buffer window
-		self.bufferWindow = curses.newwin(0, 0, 0, 0)
-		self.inputWindow = curses.newwin(0, 0, 0, 0)
+			# create the buffer window
+			(y,x) = self.screen.getmaxyx()
+			self.bufferWindow = curses.newwin(y - 1, x, 0, 0)
+			self.inputWindow = curses.newwin(1, x, y - 1, 0)
+			self.inputWindow.bkgd(" ", self.inputColor)
+			self.inputWindow.keypad(1)
+		except:
+			self.TerminateCurses()
+			raise
 	
 	def TerminateCurses(self):
 		#re-enable buffered input mode
 		curses.nocbreak()
 		# disable special key parsing (why?)
-		self.screen.keypad(0)
+		#self.screen.keypad(0)
 		# re-enable echoing
 		curses.echo()
 		# restore original operating mode
@@ -45,11 +56,13 @@ class OnelinerCurses(object):
 	
 	def Resize(self):
 		(y,x) = self.screen.getmaxyx()
-		self.bufferWindow.move(0, 0)
+		self.bufferWindow.mvwin(0, 0)
 		self.bufferWindow.resize(y - 1, x)
-		self.inputWindow.move(y - 1, 0)
+		self.bufferWindow.noutrefresh()
+		self.inputWindow.mvwin(y - 1, 0)
 		self.inputWindow.resize(1, x)
-		self.screen.refresh()
+		self.inputWindow.noutrefresh()
+		self.screen.doupdate()
 	
 	def Run(self):
 		#curses.wrapper(self.RunImpl)
@@ -57,16 +70,29 @@ class OnelinerCurses(object):
 			try:
 				# process input
 				ch = self.inputWindow.getch()
-				if ch == curses.KEY_RESIZE:
+				self.bufferWindow.addstr(str(ch)+"\n")
+				self.bufferWindow.refresh()
+				if ch == ord('q'):
+					return
+				elif ch == curses.KEY_RESIZE:
 					self.Resize()
 				elif ch == curses.KEY_UP:
 					self.CommandHistoryPrev()
 				elif ch == curses.KEY_DOWN:
 					self.CommandHistoryNext()
-				elif curses.ASCII.isascii(ch):
-					self.inputWindow.addch(ch)
-					self.inputBuffer += ord(ch)
-			except Exception:
+				elif ch == ord("\n"): # enter
+					if len(self.inputBuffer) > 0:
+						self.bufferWindow.addstr(self.inputBuffer + "\n")
+						self.bufferWindow.refresh()
+					self.CommandEnter()
+				elif ch == curses.KEY_BACKSPACE or ch == 127: # backspace
+					self.inputBuffer = self.inputBuffer[:-1]
+					self.inputWindow.clear()
+					self.inputWindow.addstr(self.inputBuffer)
+				elif ch >= 32 and ch <= 255:
+					self.inputWindow.addch(ch, self.inputColor)
+					self.inputBuffer += chr(ch)
+			except:
 				self.TerminateCurses()
 				raise
 	
@@ -75,12 +101,28 @@ class OnelinerCurses(object):
 			self.commandIndex -= 1
 			self.inputWindow.clear()
 			self.inputWindow.addstr(self.commandHistory[self.commandIndex])
+			self.inputBuffer = self.commandHistory[self.commandIndex]
 	
 	def CommandHistoryNext(self):
-		if self.commandIndex < len(self.commandHistory):
+		if self.commandIndex < len(self.commandHistory) - 1:
 			self.commandIndex += 1
 			self.inputWindow.clear()
 			self.inputWindow.addstr(self.commandHistory[self.commandIndex])
+			self.inputBuffer = self.commandHistory[self.commandIndex]
+		elif self.commandIndex == len(self.commandHistory) - 1:
+			self.commandIndex += 1
+			self.inputWindow.clear()
+			self.inputBuffer = ""
+			
+	
+	def CommandEnter(self):
+		if len(self.inputBuffer) == 0:
+			return
+		self.commandHistory.append(self.inputBuffer)
+		# set index to the next free slot
+		self.commandIndex = len(self.commandHistory)
+		self.inputBuffer = ""
+		self.inputWindow.clear()
 	
 	def RunImpl(self, key):
 		pass
